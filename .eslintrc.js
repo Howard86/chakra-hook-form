e4935830ba1d88567355640da9f60cf8958b0d0e
Howard Tai <howard@howardism.dev>
2022-03-18 23:53:08 +0800

    refactor: set up new eslint rules with fixes

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+};
