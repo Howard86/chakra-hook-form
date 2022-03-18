cb682187b1cb7a94c5656a10791c44af0758ab73
Howard Tai <howard@howardism.dev>
2022-03-19 01:03:01 +0800

    test: update test with testing library react

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import { Default as Thing } from '../stories/Thing.stories';
 
 describe('Thing', () => {
@@ -7,5 +8,16 @@ describe('Thing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Thing />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('display default message', () => {
+    const { getByText } = render(<Thing />);
+    expect(getByText('the snozzberries taste like snozzberries')).toBeTruthy();
+  });
+
+  it('display injected text', () => {
+    const { getByText } = render(<Thing>Test</Thing>);
+
+    expect(expect(getByText('Test'))).toBeTruthy();
   });
 });
