78c9e0056c019e425c323787e054aec48e073995
Howard Tai <howard@howardism.dev>
2022-03-20 10:25:19 +0800

    chore: update example

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,68 @@
 import 'react-app-polyfill/ie11';
-import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Button, ChakraProvider, Container, VStack } from '@chakra-ui/react';
 
-const App = () => {
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FormInput } from '../src';
+
+const defaultValues = {
+  username: '',
+  password: '',
+};
+
+const LoginForm = () => {
+  const [formState, setFormState] = useState(defaultValues);
+  const form = useForm({ defaultValues });
+
   return (
-    <div>
-      <Thing />
-    </div>
+    <VStack
+      as="form"
+      spacing={2}
+      onSubmit={
+        form.handleSubmit((state) => {
+          setFormState(state);
+        }) as VoidFunction
+      }
+    >
+      <FormInput
+        label="username"
+        name="username"
+        register={form.register}
+        errors={form.formState.errors}
+      />
+      <FormInput
+        type="password"
+        label="password"
+        name="password"
+        register={form.register}
+        errors={form.formState.errors}
+        options={{
+          required: 'password is required',
+          min: {
+            message: 'password has invalid format',
+            value: 6,
+          },
+        }}
+        helperText="Please enter more than 6 digits password"
+      />
+      <Button type="submit">Submit</Button>
+      {form.formState.isSubmitSuccessful && (
+        <>
+          <p data-testid="result-username">{formState.username}</p>
+          <p data-testid="result-password">{formState.password}</p>
+        </>
+      )}
+    </VStack>
   );
 };
+
+const App = () => (
+  <ChakraProvider>
+    <Container maxW="container.sm" centerContent>
+      <LoginForm />
+    </Container>
+  </ChakraProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
